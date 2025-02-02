ff1de4b7ef7974e80d53bc5c330b71dc8f0462c3
ranselorm <ranselorm@outlook.com>
2025-02-02 18:54:34 +0000

    Change hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section
       className="relative h-screen flex items-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/images/hero.jpg')" }}
+      style={{ backgroundImage: "url('/images/hero2.jpg')" }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-75"></div>
       <div className="container mx-auto px-4">
